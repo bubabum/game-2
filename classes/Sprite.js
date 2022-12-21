18fd694f240464fa19500ac09bf21595c159c7d5
mykola <bubabum.drt@gmail.com>
2022-12-21 22:30:49 +0200

    sprite class updated

--- classes/Sprite.js
+++ classes/Sprite.js
@@ -1,25 +1,27 @@
 class Sprite {
-   constructor(options) {
-      this.frameWidth = options.frameWidth;
-      this.frameHeight = options.frameHeight;
-      this.img = new Image();
-      this.img.src = options.imgSource;
-      this.frameMap = options.frameMap;
-      this.currentFrame = 0;
-      this.maxFrames = 22;
-      this.static = options.static;
-      this.frameElapsed = 0;
-      this.frameElapsedBuffer = 5;
-   }
-   draw() {
-      ctx.strokeRect(50, 50, this.frameWidth, this.frameHeight);
-      ctx.drawImage(this.img, Math.floor(this.currentFrame) * this.frameWidth, 0, this.frameWidth, this.frameHeight, 50, 50, this.frameWidth, this.frameHeight);
-      this.frameElapsed++
-      if (this.frameElapsed === this.frameElapsedBuffer) {
-         this.frameElapsed = 0;
-         this.currentFrame++
-      }
-      if (this.currentFrame > this.maxFrames - 1) this.currentFrame = 0;
-
-   }
+	constructor({ position, frameWidth, frameHeight, imgSource, animation = null }) {
+		this.position = position;
+		this.frameWidth = frameWidth;
+		this.frameHeight = frameHeight;
+		this.img = new Image();
+		this.img.src = imgSource;
+		this.currentFrame = 0;
+		this.maxFrames = 22;
+		this.animation = animation;
+		this.frameElapsed = 0;
+		this.frameElapsedBuffer = 5;
+	}
+	draw() {
+		ctx.strokeRect(50, 50, this.frameWidth, this.frameHeight);
+		ctx.drawImage(this.img, Math.floor(this.currentFrame) * this.frameWidth, 0, this.frameWidth, this.frameHeight, 50, 50, this.frameWidth, this.frameHeight);
+		this.frameElapsed++
+		if (this.frameElapsed === this.frameElapsedBuffer) {
+			this.frameElapsed = 0;
+			this.currentFrame++
+		}
+		if (this.currentFrame > this.maxFrames - 1) this.currentFrame = 0;
+	}
+	update() {
+		this.draw();
+	}
 }
--- script.js
+++ script.js
@@ -5,8 +5,7 @@ canvas.width = 1024;
 canvas.height = 576;
 
 let keyHandler = new KeyHandler();
-let options = { frameWidth: 64, frameHeight: 64, frameMap: {}, imgSource: './img/knight.png', static: false }
-let knight = new Sprite({ frameWidth: 144, frameHeight: 64, frameMap: {}, imgSource: './img/knight2.png', static: false });
+let knight = new Sprite({ frameWidth: 144, frameHeight: 64, frameMap: {}, imgSource: './img/knight2.png', animation: true });
 console.log(knight);
 
 function animation() {
